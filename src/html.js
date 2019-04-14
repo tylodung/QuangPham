59c3af3d625ae9a1d8f397b38db9d53d43e589d4
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-04-14 14:58:48 +0700

    up

--- src/html.js
+++ src/html.js
@@ -68,7 +68,7 @@ export default class HTML extends Component {
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub-7368784364399002',
+    google_ad_client: 'ca-pub-7961982735432199',
     enable_page_level_ads: true
   });
   `;
